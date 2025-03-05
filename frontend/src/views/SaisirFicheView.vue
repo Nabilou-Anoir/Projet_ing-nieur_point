2e2406ddeb4143aeaae4e514302a8a58f8d5ccd3
ANOIR <nabilou.anoir@etud.univ-jfc.fr>
2025-03-05 02:54:06 +0100

    backen et frontend propre

---
+++ frontend/src/views/SaisirFicheView.vue
@@ -0,0 +1,10 @@
+<!-- src/views/SaisirFicheView.vue -->
+<template>
+  <div>
+    <h2>Saisir une fiche</h2>
+    <p>Formulaire de saisie d'une fiche...</p>
+  </div>
+</template>
+
+<script setup>
+</script>
